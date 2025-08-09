d710564142405bfe930fb5b0eb63ddf6adef9fbb
Viviane <viane.fmar@gmail.com>
2025-08-08 22:20:58 -0300

    modifiquei os arquivos package.json e index.js, e criei a pasta utils na pasta 04-esm-modules

---
+++ 04-esm-modules/src/utils/api.js
@@ -0,0 +1,9 @@
+//no ES tb podemos usar arrow function / exportação direta
+export const key = {
+    value: "2323jklfds",
+    permission: "admin",
+};
+
+export const getDataFromApi = () => {
+    console.log("dados da api sendo buscados...");
+}
---
+++ 04-esm-modules/src/utils/database.js
@@ -0,0 +1,15 @@
+const databaseType = {
+    userType: "admin",
+    typeData: "datalocal",
+};
+
+async function connectToDatabase(dataName){
+    //lógica de conexão
+    console.log(`conectado ao banco ${dataName}`);
+}
+
+async function disconnectDatabase(){
+    console.log("desconectando do banco de dados");
+}
+
+export {connectToDatabase, disconnectDatabase, databaseType};
